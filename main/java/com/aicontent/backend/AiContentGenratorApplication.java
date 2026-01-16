a8e228c237d94cd54d0122910947940ba799503f
2910shri <144451176+2910shri@users.noreply.github.com>
2026-01-16 18:11:17 +0530

    Add files via upload

---
+++ main/java/com/aicontent/backend/AiContentGenratorApplication.java
@@ -0,0 +1,13 @@
+package com.aicontent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiContentGenratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiContentGenratorApplication.class, args);
+	}
+
+}
---
+++ test/java/com/aicontent/backend/AiContentGenratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicontent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiContentGenratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
